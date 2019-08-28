d9dfc74824fe59420b3bc785253762c63a87f0cd
kj-takeda <tktk19831025@gmail.com>
2019-08-29 02:06:23 +0900

    targetモデル作成・マイグレ実行

--- db/migrate/20190828164810_create_targets.rb
+++ db/migrate/20190828164810_create_targets.rb
@@ -1,7 +1,12 @@
 class CreateTargets < ActiveRecord::Migration[5.2]
   def change
     create_table :targets do |t|
-      t.string 
+      t.string :genre
+      t.text :title
+      t.text :text
+      t.text :url
+      t.string :img
+      t.references :user
       t.timestamps
     end
   end
