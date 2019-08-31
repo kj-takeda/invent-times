2a5f771f1cf81f7ec5554837aa33eb806c9f8dce
kj-takeda <tktk19831025@gmail.com>
2019-09-01 05:08:21 +0900

    userテーブルからinterestカラム削除

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_31_194018) do
+ActiveRecord::Schema.define(version: 2019_08_31_200520) do
 
   create_table "howtos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.text "comment"
@@ -52,7 +52,6 @@ ActiveRecord::Schema.define(version: 2019_08_31_194018) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string "interest"
     t.string "name"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
