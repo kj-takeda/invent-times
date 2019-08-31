499191988263cba0d36e0fb86d5ae14bc5b88b4a
kj-takeda <tktk19831025@gmail.com>
2019-09-01 05:03:03 +0900

    アソシエーション

--- app/models/howto.rb
+++ app/models/howto.rb
@@ -1,2 +1,4 @@
 class Howto < ApplicationRecord
+  belongs_to :user
+  belongs_to :target
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,8 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :targets
+  has_many :howtos
+  has_many :periods
 end
