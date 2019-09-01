8547773e7df260a8b136e52e9cd5383e851985d5
kj-takeda <tktk19831025@gmail.com>
2019-09-01 14:16:49 +0900

    periodコントローラにストロングパラを作成　CSSを調整

--- app/controllers/periods_controller.rb
+++ app/controllers/periods_controller.rb
@@ -1,5 +1,7 @@
 class PeriodsController < ApplicationController
   
+  before_action :authenticate_user!
+
   def new
     @period = Period.new
   end
@@ -10,7 +12,7 @@ class PeriodsController < ApplicationController
 
   private
   def period_params
-    params.require(:period).permit(:time)
+    params.require(:period).permit(:time).merge(user_id: current_user.id)
   end
 
 end
