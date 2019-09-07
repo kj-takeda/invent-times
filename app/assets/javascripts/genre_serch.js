832633d13ba9a42ba3aca319632a151529ec466d
kj-takeda <tktk19831025@gmail.com>
2019-09-07 17:59:59 +0900

    コメントアウトを追記

--- app/assets/javascripts/genre_serch.js
+++ app/assets/javascripts/genre_serch.js
@@ -1,14 +1,14 @@
 jQuery(document).on('turbolinks:load', function(){
-$(function(){
+$(function(){} //特徴ボタンを押せば特徴検索が現れジャンル検索が消える
   $('.index__serch__top__btn.no2').click(function () {
-    $('.none2').show();
-    $('.none1').hide();
+    $('.none2').show(); //特徴検索ボタンが青くなる
+    $('.none1').hide(); //ジャンル検索ボタンがグレイになる
     $(this).css('background' , '#668ad8' , 'border-bottom' , '36528c' , 'border-right' , 'solid #5375bd')
     $('.no1').css('background' , 'grey' , 'border-bottom' , 'grey' , 'border-right' , 'grey')
   })
   $('.index__serch__top__btn.no1').click(function () {
-    $('.none1').show();
-    $('.none2').hide();
+    $('.none1').show(); //ジャンル検索ボタンを押せば特徴検索が現れジャンル検索が消える
+    $('.none2').hide(); //ジャンル検索ボタンがグレイになる
     $(this).css('background' , '#668ad8' , 'border-bottom' , 'solid 36528c' , 'border-right' , 'solid #5375bd')
     $('.no2').css('background' , 'grey' , 'border-bottom' , 'grey' , 'border-right' , 'grey')
   })
